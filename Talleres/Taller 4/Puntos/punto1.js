e814b614e90a77294752757fbc49f87988b04a85
di3go0-0 <d.rincon@utp.edu.co>
2024-10-04 20:51:23 -0500

    Refactored all funtions to arrow functions

--- Talleres/Taller 4/Puntos/punto1.js
+++ Talleres/Taller 4/Puntos/punto1.js
@@ -1,5 +1,5 @@
 
-function multiplicationTable (Number, Limit) {
+const multiplicationTable = (Number, Limit) => {
     let table = [];
     for (let i = 1; i <= Limit; i++) {
         table.push(Number * i);
@@ -9,7 +9,7 @@ function multiplicationTable (Number, Limit) {
 
 // console.log(multiplicationTable(2, 4));
 
-function showMultiplicationTable(Number, Limit){
+const showMultiplicationTable = (Number, Limit) => {
     let table = multiplicationTable(Number, Limit);
     for (let i = 0; i < table.length; i++) {
         console.log(`${Number} X ${i + 1} = ${table[i]}`);
