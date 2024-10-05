630ccdb50f3126a5b293df93b88fc3ba599d0a4a
di3go0-0 <d.rincon@utp.edu.co>
2024-10-04 20:51:45 -0500

    Refactored all funtions to arrow functions

--- Talleres/Taller 4/Puntos/punto3.js
+++ Talleres/Taller 4/Puntos/punto3.js
@@ -1,4 +1,4 @@
-function leapYear(Year1, Year2){
+const leapYear = (Year1, Year2)=>{
     let leapYearCount = 0;
     for(let i = Year1; i <= Year2; i++){
         if(i % 4 == 0 && (i % 100 != 0 || i % 400 == 0)){
