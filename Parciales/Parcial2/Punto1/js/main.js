c40f0bb11b07078d1d7d15578277c3e9f1da25c9
di3go0-0 <d.rincon@utp.edu.co>
2024-10-22 19:29:14 -0500

    Completed Point 1 Partal 2

--- Parciales/Parcial2/Punto1/js/main.js
+++ Parciales/Parcial2/Punto1/js/main.js
@@ -3,7 +3,7 @@ let lienzo = canvas.getContext('2d');
 
 const dibujarLinea = (xinicial, yinicial, xfinal, yfinal) => { 
     lienzo.beginPath();
-    lienzo.strokeStyle = "Black";
+    lienzo.strokeStyle = "Grey";
     lienzo.lineWidth = 5;
 
     lienzo.moveTo(xinicial, yinicial);
@@ -13,26 +13,37 @@ const dibujarLinea = (xinicial, yinicial, xfinal, yfinal) => {
     lienzo.closePath();
 }
 
-const LimpiarCanvas = () =>{
-    lienzo.clearRect(0, 0, canvas.width, canvas.height);
+const dibujarForma = (points, color) => {
+    lienzo.beginPath();
+    lienzo.moveTo(points[0][0], points[0][1]); // Moverse al primer punto
+    for (let i = 1; i < points.length; i++) {
+        lienzo.lineTo(points[i][0], points[i][1]); // Dibujar las líneas entre los puntos
+    }
+    lienzo.closePath(); // Cerrar el camino
+    lienzo.fillStyle = color; // Establecer color de relleno
+    lienzo.fill(); // Rellenar la figura
+    lienzo.stroke(); // Dibujar el borde si lo deseas
 }
 
-const RellenarFigura = (color) => {
-  lienzo.fillStyle = color;
-  lienzo.fill();
+const LimpiarCanvas = () =>{
+    lienzo.clearRect(0, 0, canvas.width, canvas.height);
 }
 
 
 Punto1 = () => {
   LimpiarCanvas();
+
+  const Bordes = [[500,100], [673.205,200], [846.410,500], [846.410, 700], [500,900], [153.5898,700], [153.5898,300], [500,100]];
+  dibujarForma(Bordes, "black");
+  
   //Parte Superior
-  dibujarLinea(500, 100, 673.205, 200)
-  dibujarLinea(500, 100, 153.5898, 300)
-  dibujarLinea(153.5898, 300,326.7945,400)
+  dibujarLinea(500, 100, 673.205, 200) // Borde 
+  dibujarLinea(500, 100, 153.5898, 300) //borde
+  dibujarLinea(153.5898, 300,326.7945,400) 
   dibujarLinea(673.205, 200, 326.7945, 400)
 
   //Lado Izquierdo
-  dibujarLinea(153.5898, 300, 153.5898, 700)
+  dibujarLinea(153.5898, 300, 153.5898, 700) //borde
   dibujarLinea(326.7945, 400, 326.7945, 600)
   dibujarLinea(153.5898, 700, 500, 900)
   dibujarLinea(326.7945, 600, 500, 700)
@@ -40,20 +51,26 @@ Punto1 = () => {
 
   //parte in ferior
   dibujarLinea(500, 700, 846.410, 500)
-  dibujarLinea(500, 900, 846.410, 700)
-  dibujarLinea(846.410, 700, 846.410, 500)
+  dibujarLinea(500, 900, 846.410, 700) //Borde
+  dibujarLinea(846.410, 700, 846.410, 500) //Borde
 
   //parte medio
-  dibujarLinea(846.410, 500, 673.205, 200)
+  dibujarLinea(846.410, 500, 673.205, 200) //Borde
   dibujarLinea(326.7945, 600, 500, 500)
   dibujarLinea(500, 500, 673.205, 600)
   dibujarLinea(500, 500, 500,300)
   dibujarLinea(500, 300, 673.205,600)
-  RellenarFigura("blue");
+
+
 }
 
 Punto2 = () => {
+
   LimpiarCanvas();
+
+  const Bordes = [[500,100], [846.41, 300], [846.410,500], [846.410, 700], [500,900], [153.5898,700], [153.5898,300], [500,100]];
+  dibujarForma(Bordes, "blue");
+  
   //Parte Superior
   dibujarLinea(500, 100, 846.41, 300)
   dibujarLinea(500, 100, 153.5898, 300)
@@ -85,6 +102,10 @@ Punto2 = () => {
 
 Punto3 = () => {
   LimpiarCanvas();
+
+  const Bordes = [[500,100], [673.205,200], [846.410,500], [846.410, 700], [500,900], [153.5898,700], [153.5898,300], [500,100]];
+  dibujarForma(Bordes, "red");
+
   //Parte Superior
   dibujarLinea(500, 100, 673.205, 200)
   dibujarLinea(500, 100, 153.5898, 300)
@@ -117,6 +138,10 @@ Punto3 = () => {
 
 Punto4 = () => {
   LimpiarCanvas();
+
+  const Bordes = [[500,100], [673.205,200], [673.205, 400], [846.410, 500],[846.410, 700], [500,900], [153.5898,700], [153.5898,300], [500,100]];
+  dibujarForma(Bordes, "Yellow");
+
   //Parte Superior
   dibujarLinea(500, 100, 673.205, 200)
   dibujarLinea(500, 100, 153.5898, 300)
